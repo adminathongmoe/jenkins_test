fe343bfdc8a16b4eed440e34a7e2322d591128f9
admin <admin@hongmoe.com>
2020-09-17 13:41:25 +0800

    change folder

---
+++ Assets/Editor/BuildToGradle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildToGradle
+{
+    [MenuItem("BuildToGradle/Build")]
+    public static void BuildAS()
+    {
+        PlayerSettings.productName = "UnityTest";
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
+
+        // 设置打包场景
+        string launchScene = "Assets/Scenes/SampleScene.unity";
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.locationPathName = "../AndroidProject";
+        buildPlayerOptions.options = BuildOptions.None;
+        buildPlayerOptions.options |= BuildOptions.AcceptExternalModificationsToPlayer;
+        buildPlayerOptions.scenes = new string[] { launchScene };
+        buildPlayerOptions.target = BuildTarget.Android;
+
+        // 开始打包
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
